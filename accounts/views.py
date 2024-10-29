6dd997a9986c3d45eaafabee77c2056ac2860810
keyla <ktacuabe@udc.edu.ar>
2024-10-29 19:58:44 -0300

    Implementación básica de registro e inicio de sesión

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
-    return HttpResponse('Hola')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            form.save()
+            messages.success(request, f'La cuenta de {username} se ha creado con exito!')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+        return render(request, 'registration/registro.html', {'form':form})
+
+        
 
